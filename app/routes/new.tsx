303ce8222b01f906492a0781f485fa9b6dcca12a
Shoubhit Dash <shoubhit2005@gmail.com>
2022-11-07 23:47:10 +0530

    feat: form done maybe

--- app/routes/new.tsx
+++ app/routes/new.tsx
@@ -1,17 +1,53 @@
-import { Form } from "@remix-run/react";
+import { ActionArgs, json } from "@remix-run/node";
+import { Form, useActionData, useTransition } from "@remix-run/react";
+import invariant from "tiny-invariant";
+
+type ActionData =
+  | {
+      header: string | null;
+      content: string | null;
+    }
+  | undefined;
+
+export const action = async ({ request }: ActionArgs) => {
+  const formData = await request.formData();
+
+  const header = formData.get("header");
+  const content = formData.get("content");
+
+  const errors: ActionData = {
+    header: header ? null : "Header is required",
+    content: content ? null : "Content is required",
+  };
+
+  const hasErrors = Object.entries(errors).some((errorMsg) => errorMsg);
+  if (hasErrors) return json<ActionData>(errors);
+
+  invariant(typeof header === "string", "header must be a string");
+  invariant(typeof content === "string", "content must be a string");
+
+  return {};
+};
 
 const NewPage = () => {
+  const errors = useActionData() as ActionData;
+  const transition = useTransition();
+  const loading = Boolean(transition.submission);
+
   return (
     <main className="flex flex-col items-center p-6 text-left">
       <h1 className="pb-6 text-3xl font-extrabold text-gray-200">
         Write a New Letter
       </h1>
 
-      <Form>
+      <Form method="post">
         <div className="flex flex-col gap-y-4">
           <label htmlFor="header" className="font-semibold">
             Header
           </label>
+          {errors?.header ? (
+            <span className="text-sm text-red-500">{errors.header}</span>
+          ) : null}
           <input
             name="header"
             id="header"
@@ -22,6 +58,9 @@ const NewPage = () => {
           <label htmlFor="content" className="font-semibold">
             Content
           </label>
+          {errors?.content ? (
+            <span className="text-sm text-red-500">{errors.content}</span>
+          ) : null}
           <textarea
             name="content"
             id="content"
@@ -29,7 +68,15 @@ const NewPage = () => {
             rows={15}
             placeholder="Your letter content goes here"
             className="w-96 rounded  border border-neutral-800 bg-neutral-900 px-4 py-2 placeholder:text-neutral-500 focus:outline-none"
-          ></textarea>
+          />
+
+          <button
+            type="submit"
+            className="rounded border border-neutral-700 bg-neutral-800 p-2 px-4 disabled:cursor-not-allowed disabled:opacity-50"
+            disabled={loading}
+          >
+            Next
+          </button>
         </div>
       </Form>
     </main>
