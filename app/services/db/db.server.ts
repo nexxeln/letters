df72c4f7b44ddcbd815c07660f685bccd59dcaf2
Shoubhit Dash <shoubhit2005@gmail.com>
2022-11-07 20:05:37 +0530

    feat: set up prisma

---
+++ app/services/db/db.server.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+
+let db: PrismaClient;
+
+declare global {
+  var __db: PrismaClient | undefined;
+}
+
+if (process.env.NODE_ENV === "production") {
+  db = new PrismaClient();
+} else {
+  if (!global.__db) {
+    global.__db = new PrismaClient({
+      log:
+        process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+    });
+  }
+  db = global.__db;
+}
+
+export { db };
