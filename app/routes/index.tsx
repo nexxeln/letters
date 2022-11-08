0e7294bbad25ce5fec4f4c5fc551724a33a44a19
Shoubhit Dash <shoubhit2005@gmail.com>
2022-11-08 23:13:43 +0530

    feat: add links to me and github

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,6 +6,28 @@ export default function Index() {
       <Link to="/new" className="link text-3xl font-black">
         Write a Letter
       </Link>
+
+      <footer className="fixed bottom-2 left-2 text-xs text-gray-400">
+        Created by{" "}
+        <a
+          href="https://www.nexxel.dev/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-300 underline"
+        >
+          Shoubhit Dash
+        </a>
+        . Open source on{" "}
+        <a
+          href="https://github.com/nexxeln/letters"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-300 underline"
+        >
+          GitHub
+        </a>
+        .
+      </footer>
     </main>
   );
 }
