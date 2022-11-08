26c189b27d2a98bcd5da0db8ce70669d6fe78c20
Shoubhit Dash <shoubhit2005@gmail.com>
2022-11-08 20:41:20 +0530

    feat: seo

--- app/root.tsx
+++ app/root.tsx
@@ -12,8 +12,13 @@ import styles from "~/styles/generated.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
+  title: "Write a Letter",
+  description: "Write a Letter. Make someone happy.",
+  "twitter:image": "/banner.png",
+  "twitter:card": "summary_large_image",
+  "twitter:title": "Write a Letter",
+  "twitter:description": "Write a Letter. Make someone happy.",
 });
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
@@ -24,6 +29,10 @@ export default function App() {
       <head>
         <Meta />
         <Links />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>✉️</text></svg>"
+        />
       </head>
       <body>
         <Outlet />
