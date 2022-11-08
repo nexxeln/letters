485b77bf57613e2474a522e655629139b87c9da9
Shoubhit Dash <shoubhit2005@gmail.com>
2022-11-08 14:08:08 +0530

    feat: better index page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,9 +2,11 @@ import { Link } from "@remix-run/react";
 
 export default function Index() {
   return (
-    <main className="">
-      <h1 className="">Letters</h1>
-      <Link to="/new">Create New</Link>
+    <main className="flex h-screen flex-col items-center justify-center">
+      <Link to="/new" className="text-3xl font-black">
+        Write a Letter
+      </Link>
+      <span className="">(this is a link btw)</span>
     </main>
   );
 }
