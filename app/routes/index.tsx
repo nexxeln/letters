f300a167f7e4679556998de583b7a41180fc7c30
Shoubhit Dash <shoubhit2005@gmail.com>
2022-11-08 19:40:10 +0530

    feat: im a css god myself

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,10 +3,9 @@ import { Link } from "@remix-run/react";
 export default function Index() {
   return (
     <main className="flex h-screen flex-col items-center justify-center">
-      <Link to="/new" className="text-3xl font-black">
+      <Link to="/new" className="link text-3xl font-black">
         Write a Letter
       </Link>
-      <span className="">(this is a link btw)</span>
     </main>
   );
 }
